b51887a591460ec2040cea68ff31312f42001c63
samuelmaderson <samuel.maderson@gmail.com>
2023-11-29 23:45:11 -0300

    lesson 2 finished

--- lesson 2/api_integration.py
+++ lesson 2/api_integration.py
@@ -5,42 +5,54 @@ import os
 
 load_dotenv()
 
-openai.api_key = os.getenv("OPENAI_API_KEY")    
-prompt_system = """
-    You're a product categorization
-    When you receive a user request you've to should between these categories below
-    ### Categories
-        Beauty
-        Sports
-        Finantial
-        Others
-    ### Question
-        Category of Basketball?
-    ### Output
-        Sports
+def products_categorizer(product_name: str, product_categories: str):
 
-"""
-response = openai.ChatCompletion.create(
-    
-    messages = [
-        {
-            "role": "system",
-            "content": prompt_system
-        },
-        {
-            "role": "user",
-            "content": "Brush Teeth"
-        }
-    ],
+    openai.api_key = os.getenv("OPENAI_API_KEY")    
+    prompt_system = f"""
+        You're a product categorizer.
+        You must choose a category from the list below:
+        If the categories provided aren't valid categories, respond with "I can't help you with that"
+        ##### List of valid categories
+        {product_categories}
+        ##### Example
+        tennis ball
+        sports
 
-    model="gpt-3.5-turbo",
-    temperature=1,
-    max_tokens=256,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0,
-    n=5
-)
+    """
+    response = openai.ChatCompletion.create(
+        
+        messages = [
+            {
+                "role": "system",
+                "content": prompt_system
+            },
+            {
+                "role": "user",
+                "content": product_name
+            }
+        ],
 
-for i in range(0,5):
-    print(response.choices[i].message.content, end="\n-------------------\n")
+        model="gpt-3.5-turbo",
+        temperature=1,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+    )
+
+    print(response.choices[0].message.content)
+
+
+while True:
+    try:
+        product_name = input("Product name: ")
+        product_categories = input("Product categories: ")
+
+        products_categorizer(product_name, product_categories)
+        
+    except KeyboardInterrupt:
+        print("Exiting...")
+        break
+    except Exception as e:
+        print(e)
+        break
