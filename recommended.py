2a71890174f0edb84f0ccc9c09532cc5a8cf7b31
samuelmaderson <samuel.maderson@gmail.com>
2023-12-10 20:35:35 -0300

    finished products recommendation

--- recommended.py
+++ recommended.py
@@ -9,23 +9,23 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 def load_data(filename: str):
 
     try:
-        print(f"\033[1;32m [+] \033[0mLoading data from {filename}")
+        print(f"\033[1;32m[+] \033[0mLoading data from {filename}")
         
         with open(filename, 'r') as file:
             return file.read()
     except IOError as e:
-        print(f"\033[1;31m [-] \033[0mError loading data from {filename}: {e}")
+        print(f"\033[1;31m[-] \033[0mError loading data from {filename}: {e}")
 
 
 def save_data(filename: str, data: str):
 
     try:
-        print(f"\033[1;32m [+] \033[0mSaving data to {filename}")
+        print(f"\033[1;32m[+] \033[0mSaving data to {filename}")
 
         with open(f'./data/review-{filename}.txt', 'w') as file:
             file.write(data)
     except IOError as e:
-        print(f"\033[1;31m [-] \033[0mError saving data to {filename}: {e}")
+        print(f"\033[1;31m[-] \033[0mError saving data to {filename}: {e}")
 
 
 def analyze_profiles(prompt_user: str):
@@ -64,34 +64,35 @@ def analyze_profiles(prompt_user: str):
             presence_penalty=0
         )
 
-
         return response.choices[0].message.content
     
     except openai.error.APIConnectionError as e:
-        print(f"\033[1;31m [-] \033[0mError connecting to OpenAI API: {e}")
+        print(f"\033[1;31m[-] \033[0mError connecting to OpenAI API: {e}")
     except Exception as e:
-        print(f"\033[1;31m [-] \033[0mError analyzing profiles: {e}")
+        print(f"\033[1;31m[-] \033[0mError analyzing profiles: {e}")
 
 
 
 
-def recommend_products(prompt_user: str):
+def recommend_products(profile: str, products_list: str):
 
-    prompt_system = """
-        You're a products recommender.
+    prompt_system = f"""
+        You are a product recommender.
+          Consider the following profile: {profile}
+          We recommend 3 products from the list of valid products that are suitable for the profile provided.
+        
+          #### List of products valid for recommendation
+          {products_list}
+        
+          Use English Language
+          The output should only be the name of the products recommended in bullet points, like this:
 
-        Based on this input:
+          - product1
+          - product2
+          - product3
 
-        client name
-        categories
-
-        output with 3 products related to these categories. only say the product name with a good description of the product but at same time, try to convince the client to buy it. Moreover, before talking about the product, call the client by the name and try to be very kind and aware of what he needs.
-
-        # For instance:
-        Product: 
-        Description: 
-
-        Use English language.
+          as I said, don't say at the beginning, "Based on product" or "I recommend", just tell me the 3 product.
+          also output the product in english language.
     """
 
     response = openai.ChatCompletion.create(
@@ -101,10 +102,6 @@ def recommend_products(prompt_user: str):
             {
                 "role": "system",
                 "content": prompt_system
-            },
-            {
-                "role": "user",
-                "content": prompt_user
             }
         ],
         temperature=1,
@@ -117,16 +114,19 @@ def recommend_products(prompt_user: str):
     return response.choices[0].message.content
 
 
+
 if __name__ == '__main__':
 
-    product_list = load_data('./data/lista_de_compras_10_clientes.csv')
-    print("\033[1;32m [+] \033[0mAnalyzing...")
-    processsed_data = analyze_profiles(product_list)
-    json_data = json.loads(processsed_data)
-    for client in json_data['clients']:
-        print(f"Running on client: {client['name']}")
-        response = recommend_products(f"Client: {client['name']}\nCategories: {client['profile']}")
-        save_data(client['name'], response)
+    purchased_list = load_data('./data/lista_de_compras_10_clientes.csv')
+    product_list = load_data('./data/lista_de_produtos.txt')
+    print("\033[1;32m[+] \033[0mAnalyzing...")
+    profiles = json.loads(analyze_profiles(purchased_list))
+
+    for client in profiles['clients']:
+        print(f"\033[1;32m[+] \033[0mRunning on client: {client['name']}\033[0m")
+        products = response = recommend_products(client['profile'], product_list)
+        print(f"\033[1;32m[+] \033[0mFinished on client: {client['name']}")
+        print(f"{products}")
         
         
         
